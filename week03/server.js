43b862dab211e102a30b3556bea6febb9c9427b3
zhaochangyuan <757831625@qq.com>
2020-08-11 20:27:23 +0800

    1. HTML解析 1 2

---
+++ week03/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    let body = [];
+
+    request.on('error', (err) => {
+        console.log(err);
+    }).on('data', (chunk) => {
+        // 这里chunk是Buffer类型，如果转成string，会导致end中 Buffer.concat(body)报错。
+        // body.push(chunk.toString());
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log('body:', body);
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end('<h1>12343</h1>');
+    });
+}).listen(8088);
+
+console.log('server started');
