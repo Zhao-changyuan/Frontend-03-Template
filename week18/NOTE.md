e61bd3bbd0408719942eed1df705af4d3df5c734
zhaochangyuan <757831625@qq.com>
2020-12-05 16:35:00 +0800

    week18

--- week18/NOTE.md
+++ week18/NOTE.md
@@ -1,1 +1,79 @@
 学习笔记
+
+# 1. 单元测试工具 | Mocha（一）
+测试工具是工具链中非常重要的一环，必需品。
+
+比较火的有Mocha、Jest。
+
+npm install --save-dev mocha
+npx mocha
+
+使用assert断言输入和输出的相等性；describe用于分组，让结构更好看。
+
+# 2. 单元测试工具 | Mocha（二）
+为了避免我们写的模块必须以node.js的方式导出。
+
+@babel/core @babel/register @babel/preset-env
+.babelrc中：
+```js
+{
+    "presets": ["@babel/preset-env"]
+}
+```
+
+node modules的脚本里面，会默认把node_modules/.bin加到自己的path里面。
+
+使用packgae.json的"scripts"
+```js
+"scripts": {
+    test": "mocha --require @babel/register"
+}
+```
+
+单元测试有一个非常重要的指标：code coverage
+# 3. 单元测试工具 | code coverage
+code coverage表示测试覆盖到了源文件里面的哪些代码。
+
+istanbuljs nyc，可以在一个复杂的文件中帮我们计算测试覆盖的比例。
+
+nyc的指标有 % Stmts、 % Branch、 % Funcs、 % Lines。
+我们主要还是看% Lines。
+
+nyc要带上babel运行的话，需要给babel和nyc互相加一个插件:
+* babel安装插件babel-plugin-istanbul
+* nyc安装插件@istanbuljs/nyc-config-babel
+
+# 4. 单元测试工具 | 对html-parser进行单元测试
+配置launch.json运行mocha:
+```js
+"runtimeArgs": [
+    "--require", "@babel/register"
+],
+"sourceMaps": true,
+"args": [],
+"program": "${workspaceFolder}/node_modules/mocha/bin/_mocha"
+```
+
+.babelrc设置：
+```js
+"sourceMaps": "inline"
+```
+
+不断npm run coverage并添加用例提高 % Lines.
+
+# 5. 单元测试工具 | 所有工具与generator的集成
+generator-toytool
+
+添加依赖：
+"@babel/core",
+"@babel/preset-env",
+"@babel/register",
+"babel-loader",
+"babel-plugin-istanbul",
+"mocha",
+"nyc"
+
+复制文件：.babelrc、.nycrc、sample-test.js
+
+webpack配置rules babel-loader
+
---
+++ week18/html-parser/test/test.js
@@ -0,0 +1,105 @@
+var assert = require('assert');
+import { parseHTML } from '../src/parser.js';
+
+describe('parse html:', function() {
+    it('<a></a>', function() {
+        let tree = parseHTML('<a></a>');
+        assert.equal(tree.children[0].tagName, 'a');
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a href="//time.geekbang.org"></a>', function() {
+        let tree = parseHTML('<a href="//time.geekbang.org"></a>');
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a href ></a>', function() {
+        let tree = parseHTML('<a href ></a>');
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a href id></a>', function() {
+        let tree = parseHTML('<a href id></a>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a href="abc" id></a>', function() {
+        let tree = parseHTML('<a href="abc" id></a>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a id=abc></a>', function() {
+        let tree = parseHTML('<a id=abc></a>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a  id=abc/>', function() {
+        let tree = parseHTML('<a  id=abc/>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a id=\'abc\'/>', function() {
+        let tree = parseHTML('<a id=\'abc\'/>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a />', function() {
+        let tree = parseHTML('<a />');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<A /> upper case', function() {
+        let tree = parseHTML('<A />');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<A/>', function() {
+        let tree = parseHTML('<A/>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<>', function() {
+        let tree = parseHTML('<>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].type, 'text');
+    });
+
+    it('text', function() {
+        let tree = parseHTML('text');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].type, 'text');
+    });
+
+    it('<a id=abc href/>', function() {
+        let tree = parseHTML('<a id=abc href/>');
+        console.log(tree);
+        assert.equal(tree.children.length, 1);
+        assert.equal(tree.children[0].children.length, 0);
+    });
+
+    it('<a id="abc"\'> throw Error', function() {
+        assert.throws(function() {
+            parseHTML('<a id="abc"\'>')
+        });
+    });
+});
